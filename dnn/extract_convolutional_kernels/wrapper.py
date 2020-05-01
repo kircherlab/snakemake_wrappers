8ad2277badce77672e38409141b94ce26f0f0d24
Max <max.schubach@googlemail.com>
2020-05-01 14:48:01 +0200

    change extract convolutonal kernels to use a product of the alphabet to generate PWMs

--- dnn/extract_convolutional_kernels/wrapper.py
+++ dnn/extract_convolutional_kernels/wrapper.py
@@ -8,6 +8,7 @@ import os
 
 import h5py
 import numpy as np
+from itertools import product
 
 
 f = h5py.File(snakemake.input[0], 'r')
@@ -15,20 +16,32 @@ f = h5py.File(snakemake.input[0], 'r')
 # must be saved under conv1/conv1/kernel:0 I have no idea if this will generalize
 kernels = np.array(f[snakemake.params[0]][snakemake.params[0]]['kernel:0'])
 
-# The question is how to normalize?I woud suggest that the kernel integers range only from -1 to 1. Otherwise we can use the min max
-#kernel_conv1_normMinMax = np.interp(kernel_conv1, (kernel_conv1.min(), kernel_conv1.max()), (0, 1))
-kernels_norm = np.interp(kernels, (-1, 1), (0, 1))
-
 # function to save a motif
 def writeMotif(file, motif, name):
+    motif = motif.T
     file.write("%s\n" % (name))
     file.write("A: "+" ".join(list(map(str,motif.tolist()[0]))) + "\n")
     file.write("C: "+" ".join(list(map(str,motif.tolist()[1]))) + "\n")
     file.write("G: "+" ".join(list(map(str,motif.tolist()[2]))) + "\n")
     file.write("T: "+" ".join(list(map(str,motif.tolist()[3]))) + "\n")
 
+# default use the best 100. can be set as second parameter
+if len(snakemake.params > 1):
+    max_n = snakemake.params[1]
+else:
+    max_n = 100
+
+# product of alphabet using the kerne size
+alphabet = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+prod = product(alphabet,repeat=kernels.shape[0])
+prod = np.array(list(prod))
+
+
 # output file. Not compressed. plain text
 with open(snakemake.output[0], 'w') as motifsOut:
     # iterate over the different kernels
-    for i in range(kernels_norm.shape[2]):
-        writeMotif(motifsOut, np.transpose(kernels_norm[:,:,i]), "kernel:%d" % i)
+    for i in range(kernels.shape[-1]):
+        res = (prod* kernels[:,:,i]).sum(2).sum(1)
+        highest_n_index = res.argsort()[-max_n:][::-1]
+        pwm = prod[highest_n_index].sum(0)/max_n
+        writeMotif(motifsOut, pwm, "kernel:%d" % i)
