d559b5eb1f7aa1e82fe058d03cd4907e8013b873
Max Schubach <max.schubach@bihealth.de>
2021-03-03 07:06:27 +0100

    creating AUC with tests

---
+++ evaluate/auc/auc.py
@@ -0,0 +1,46 @@
+import click
+import pandas as pd
+
+from sklearn import metrics
+
+# options
+@click.command()
+@click.option('--input',
+              'input_file',
+              required=True,
+              type=click.Path(exists=True, readable=True),
+              help='Input file with label and predictions')
+@click.option('--label-column',
+              'label_column',
+              required=True,
+              type=str,
+              help='Column name of the labels')
+@click.option('--positive-label',
+              'pos_label',
+              required=True,
+              type=int,
+              help='The positive label')
+@click.option('--prediction-column',
+              'prediction_column',
+              required=True,
+              type=str,
+              help='Column name of the predictions')
+@click.option('--output',
+              'output_file',
+              required=True,
+              type=click.Path(writable=True),
+              help='Output file with AUCs.')
+def cli(input_file, label_column, pos_label, prediction_column, output_file):
+
+    df = pd.read_csv(input_file, delimiter="\t")
+    
+    fpr, tpr, thresholds = metrics.roc_curve(df[label_column], df[prediction_column], pos_label=pos_label)
+    auroc = metrics.auc(fpr, tpr)
+
+    precision, recall, thresholds = metrics.precision_recall_curve(df[label_column], df[prediction_column], pos_label=pos_label)
+    auprc = metrics.average_precision_score(df[label_column], df[prediction_column], pos_label=pos_label)
+
+    pd.DataFrame(data={"metric":["AUROC","AUPRC"],"value":[auroc,auprc]}).to_csv(output_file, header=True, index=False, sep="\t", float_format='%.3f')
+
+if __name__ == '__main__':
+    cli()
---
+++ evaluate/auc/tests/test_auc.py
@@ -0,0 +1,16 @@
+from click.testing import CliRunner
+import filecmp
+
+from auc import  cli
+
+def test1_auc():
+  runner = CliRunner()
+  result = runner.invoke(cli, ['--input', 'tests/input.tsv','--label-column','label','--positive-label',2,'--prediction-column',"prediction",'--output',"tests/output.tsv"])
+  assert result.exit_code == 0
+  assert filecmp.cmp("tests/output.tsv","tests/outTest1.tsv")
+
+def test2_auc():
+  runner = CliRunner()
+  result = runner.invoke(cli, ['--input', 'tests/input.tsv','--label-column','label','--positive-label',1,'--prediction-column',"prediction",'--output',"tests/output.tsv"])
+  assert result.exit_code == 0
+  assert filecmp.cmp("tests/output.tsv","tests/outTest2.tsv")
