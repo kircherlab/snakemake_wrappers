a73454a44c3dd58230c69338dd83288599c93aea
Max Schubach <max.schubach@bihealth.de>
2021-03-03 07:20:59 +0100

    flake8

--- evaluate/auc/auc.py
+++ evaluate/auc/auc.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn import metrics
 
 # options
+
+
 @click.command()
 @click.option('--input',
               'input_file',
@@ -33,14 +35,19 @@ from sklearn import metrics
 def cli(input_file, label_column, pos_label, prediction_column, output_file):
 
     df = pd.read_csv(input_file, delimiter="\t")
-    
+
     fpr, tpr, thresholds = metrics.roc_curve(df[label_column], df[prediction_column], pos_label=pos_label)
     auroc = metrics.auc(fpr, tpr)
 
-    precision, recall, thresholds = metrics.precision_recall_curve(df[label_column], df[prediction_column], pos_label=pos_label)
+    precision, recall, thresholds = metrics.precision_recall_curve(
+        df[label_column], df[prediction_column], pos_label=pos_label
+    )
     auprc = metrics.average_precision_score(df[label_column], df[prediction_column], pos_label=pos_label)
 
-    pd.DataFrame(data={"metric":["AUROC","AUPRC"],"value":[auroc,auprc]}).to_csv(output_file, header=True, index=False, sep="\t", float_format='%.3f')
+    pd.DataFrame(data={"metric": ["AUROC", "AUPRC"], "value": [auroc, auprc]}).to_csv(
+        output_file, header=True, index=False, sep="\t", float_format='%.3f'
+    )
+
 
 if __name__ == '__main__':
     cli()
