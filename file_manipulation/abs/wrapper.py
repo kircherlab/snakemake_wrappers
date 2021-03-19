062b9264cfd51c9c4da5a43e8d96114c2395cf89
Max <max.schubach@bihealth.de>
2021-03-19 10:29:53 +0100

    forgott wrapper

--- file_manipulation/abs/wrapper.py
+++ file_manipulation/abs/wrapper.py
@@ -8,10 +8,34 @@ from snakemake.shell import shell
 
 scriptFolder = os.path.dirname(os.path.abspath(__file__))
 
+class MissingParameterException(Exception):
+    """Exception raised for errors in the input.
+
+    Args:
+        Exception ([type]): Exception class cast
+
+    Attributes:
+        parameter (string): Parameter which is missing.
+    """
+
+    def __init__(self, parameter):
+        self.parameter = parameter
+
+    def __str__(self):
+        return("Parameter %s is missing!" % (self.parameter))
+
+# Checking parameters, remove else when parameter is not necessary and add a default value
+
+if "columns" in snakemake.params.keys():
+    param_columns = " ".join(["--column %s" % i for i in snakemake.params["columns"]])
+else:
+    raise MissingParameterException("columns")
+
 # running the shell
 shell(
     """
-    python  {scriptFolder}/abs.py\
+    python {scriptFolder}/abs.py \
+    {params_columns}
      --input {snakemake.input}  \
      --output {snakemake.output}
     """
