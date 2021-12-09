5d02494d1edc5e0279ef39ce5c1d72025faad1e1
Max <visze@users.noreply.github.com>
2021-12-09 11:41:39 +0100

    adding index option to conat.py
    now we are able to concat on an index.. this can append files on the right side

--- file_manipulation/concat/concat.py
+++ file_manipulation/concat/concat.py
@@ -24,23 +24,34 @@ import click
               required=False,
               type=(str, str),
               help='Adding collumns with value. can be given for each file seperately or one for all')
+@click.option('--index',
+              'index',
+              multiple=False,
+              required=False,
+              type=str,
+              help='Using the index for concat (must be the same on all files. This uses axis=1 for concat.')
 @click.option('--output',
               'output_file',
               required=True,
               type=click.Path(writable=True),
               help='Output TSV file with headers.')
-def cli(input_files, columns, output_file):
+def cli(input_files, columns, index, output_file):
     if columns:
         if len(columns) != 1 and len(input_files) != len(columns):
             raise Exception("column option must be 1 or the length of the files")
 
+    axis = 1 if index else 0
+      
     output = pd.DataFrame()
 
     for i, input_file in enumerate(input_files):
         df = pd.read_csv(input_file, sep="\t")
         if columns and len(columns) > 1:
             df[columns[i][0]] = columns[i][1]
-        output = pd.concat([output, df])
+        df.set_index(index,inplace=True) if index in df.columns
+        
+        if index:
+          output = pd.concat([output, df], axis=axis)
 
     if columns and len(columns) == 1:
         output[columns[0][0]] = columns[0][1]
