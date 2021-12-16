a12d0bec2c94b893e721eeb0a83b8aed7c6aab62
visze <max.schubach@bihealth.de>
2021-12-16 13:04:17 +0100

    adding new wrapper nucleotide composition att position

---
+++ nucleotides/composition_at_position/tests/test_composition_at_position.py
@@ -0,0 +1,57 @@
+from click.testing import CliRunner
+import filecmp
+
+from nucleotideCountPerPosition import cli
+
+
+def test1_nucleotideCountPerPosition():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--input', 'tests/input1.tsv',
+                                 '--column', 'A',
+                                 '--output', "tests/output.tsv"])
+    assert result.exit_code == 1
+
+
+def test2_nucleotideCountPerPosition():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--input', 'tests/input1.tsv',
+                                 '--column', 'A', ])
+    assert result.exit_code == 2
+
+
+def test3_nucleotideCountPerPosition():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--input', 'tests/input1.tsv',
+                                 '--column', 1,
+                                 '--output', "tests/output.tsv"])
+    assert result.exit_code == 0
+    assert filecmp.cmp("tests/output.tsv", "tests/outTest1.tsv")
+
+
+def test4_nucleotideCountPerPosition():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--input', 'tests/input1.tsv',
+                                 '--column', 2,
+                                 '--output', "tests/output.tsv"])
+    assert result.exit_code == 0
+    assert filecmp.cmp("tests/output.tsv", "tests/outTest2.tsv")
+
+
+def test5_nucleotideCountPerPosition():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--input', 'tests/input2.tsv',
+                                 '--column', "A",
+                                 '--header',
+                                 '--output', "tests/output.tsv"])
+    assert result.exit_code == 0
+    assert filecmp.cmp("tests/output.tsv", "tests/outTest1.tsv")
+
+
+def test6_nucleotideCountPerPosition():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--input', 'tests/input2.tsv',
+                                 '--column', "B",
+                                 '--header',
+                                 '--output', "tests/output.tsv"])
+    assert result.exit_code == 0
+    assert filecmp.cmp("tests/output.tsv", "tests/outTest2.tsv")
---
+++ nucleotides/composition_at_position/wrapper.py
@@ -0,0 +1,51 @@
+__author__ = "Max Schubach"
+__copyright__ = "Copyright 2021"
+__email__ = "max.schubach@bih-charite.de"
+__license__ = "MIT license"
+
+import os
+from snakemake.shell import shell
+
+scriptFolder = os.path.dirname(os.path.abspath(__file__))
+
+
+class MissingParameterException(Exception):
+    """Exception raised for errors in the input.
+
+    Args:
+        Exception ([type]): Exception class cast
+
+    Attributes:
+        parameter (string): Parameter which is missing.
+    """
+
+    def __init__(self, parameter):
+        self.parameter = parameter
+
+    def __str__(self):
+        return("Parameter %s is missing!" % (self.parameter))
+
+
+# Checking parameters, remove else when parameter
+# is not necessary and add a default value
+
+if "column" in snakemake.params.keys():
+    param_column = "--column %s" % str(snakemake.params["column"])
+else:
+    raise MissingParameterException("column")
+
+if "header" in snakemake.params.keys():
+    param_header = "--header" if snakemake.params["header"] else "--no-header"
+else:
+    param_header = "--no-header"
+
+
+# running the shell
+shell(
+    """
+    python  {scriptFolder}/nucleotideCountPerPosition.py \
+    {param_header} \
+    {param_column} \
+    --input {snakemake.input}  --output  {snakemake.output}
+    """
+)
