4cad4e4a71c08d2e5f60403faa00ad296bd18762
Max <max.schubach@bihealth.de>
2022-01-04 17:43:17 +0100

    replacement with ints

--- file_manipulation/replace/replace.py
+++ file_manipulation/replace/replace.py
@@ -4,8 +4,8 @@
 Replace entries in table
 """
 
-import sys
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 
 import click
 
@@ -13,31 +13,40 @@ import click
 
 
 @click.command()
-@click.option('--input',
-              'input_file',
-              required=True,
-              multiple=False,
-              type=click.Path(exists=True, readable=True),
-              help='Input TSV file with headers')
-@click.option('--replace',
-              'replacements',
-              multiple=True,
-              required=True,
-              type=(str,str, str),
-              help='Column, patter, repleacement')
-@click.option('--output',
-              'output_file',
-              required=True,
-              type=click.Path(writable=True),
-              help='Output TSV file with headers.')
-
+@click.option(
+    "--input",
+    "input_file",
+    required=True,
+    multiple=False,
+    type=click.Path(exists=True, readable=True),
+    help="Input TSV file with headers",
+)
+@click.option(
+    "--replace",
+    "replacements",
+    multiple=True,
+    required=True,
+    type=(str, str, str),
+    help="Column, patter, repleacement",
+)
+@click.option(
+    "--output",
+    "output_file",
+    required=True,
+    type=click.Path(writable=True),
+    help="Output TSV file with headers.",
+)
 def cli(input_file, replacements, output_file):
-    
+
     df = pd.read_csv(input_file, sep="\t")
     for replacement in replacements:
-        df[replacement[0]] = df[replacement[0]].replace(replacement[1],replacement[2])
-        
-    df.to_csv(output_file, index=False,sep="\t")
-    
-if __name__ == '__main__':
+        if is_numeric_dtype(df[replacement[0]]):
+            replacement[1] = float(replacement[1])
+    for replacement in replacements:
+        df[replacement[0]] = df[replacement[0]].replace(replacement[1], replacement[2])
+
+    df.to_csv(output_file, index=False, sep="\t")
+
+
+if __name__ == "__main__":
     cli()
