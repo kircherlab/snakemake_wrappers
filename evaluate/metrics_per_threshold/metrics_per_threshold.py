3559762ffcdf62d8793dd337de6af52638b2ffeb
visze <max.schubach@bihealth.de>
2022-02-11 10:07:34 +0100

    adding decimals to reduce computational time nice progress bar

--- evaluate/metrics_per_threshold/metrics_per_threshold.py
+++ evaluate/metrics_per_threshold/metrics_per_threshold.py
@@ -1,4 +1,5 @@
 import click
+from click import progressbar
 import pandas as pd
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import confusion_matrix
@@ -25,49 +26,54 @@ from sklearn.metrics import balanced_accuracy_score
               help='column name of the real label')
 @click.option('--positive-label',
               'positive_label',
-              required=True,
               default=1,
               type=int,
               help='column name of the positive label')
+@click.option('--descimals',
+              'decimals',
+              default=2,
+              type=int,
+              help='Threshold decimals for computation')
 @click.option('--output',
               'output_file',
               required=True,
               type=click.Path(writable=True),
               help='Final plot.')
-def cli(input_file, score_column, label_column, positive_label, output_file):
+def cli(input_file, score_column, label_column, positive_label, output_file, decimals):
 
     df = pd.read_csv(input_file, delimiter="\t")
 
     scores = df[[score_column]].iloc[:, 0]
     labels = df[[label_column]].iloc[:, 0]
 
-    thresholds = scores.unique()
+    thresholds = scores.round(decimals).unique()
     thresholds.sort()
 
     output = pd.DataFrame()
 
-    for thresh in thresholds:
-        scores_thresh = [1 if i >= thresh else 0 for i in scores]
+    with progressbar(thresholds, show_pos=True) as bar:
+        for thresh in bar:
+            scores_thresh = [1 if i >= thresh else 0 for i in scores]
 
-        # prec, rec f1 score
-        result_prec_rec = precision_recall_fscore_support(
-            labels, scores_thresh, labels=[positive_label], pos_label=positive_label)
-        result_prec_rec = [i[0] for i in result_prec_rec]
+            # prec, rec f1 score
+            result_prec_rec = precision_recall_fscore_support(
+                labels, scores_thresh, labels=[positive_label], pos_label=positive_label)
+            result_prec_rec = [i[0] for i in result_prec_rec]
 
-        # accuracy
-        result_acc = [accuracy_score(labels, scores_thresh)] + [balanced_accuracy_score(labels, scores_thresh)]
+            # accuracy
+            result_acc = [accuracy_score(labels, scores_thresh)] + [balanced_accuracy_score(labels, scores_thresh)]
 
-        # TP TN FP FN
-        classes = labels.unique().tolist()
-        classes.remove(positive_label)
-        classes = classes + [positive_label]
-        result_conf = confusion_matrix(labels, scores_thresh, labels=classes).ravel().tolist()
+            # TP TN FP FN
+            classes = labels.unique().tolist()
+            classes.remove(positive_label)
+            classes = classes + [positive_label]
+            result_conf = confusion_matrix(labels, scores_thresh, labels=classes).ravel().tolist()
 
-        result = pd.DataFrame([result_conf + result_prec_rec + result_acc], columns=[
-            "True-negatives", "False-positives", "False-negatives", "True-positives", "Precision",
-            "Recall", "F1-score", "Support", "Accuracy", "Balanced-accuracy"
-            ], index=[thresh])
-        output = output.append(result)
+            result = pd.DataFrame([result_conf + result_prec_rec + result_acc], columns=[
+                "True-negatives", "False-positives", "False-negatives", "True-positives", "Precision",
+                "Recall", "F1-score", "Support", "Accuracy", "Balanced-accuracy"
+                ], index=[thresh])
+            output = output.append(result)
 
     output["Sensitivity"] = output["True-positives"]/(output["True-positives"]+output["False-negatives"])
     output["Specificity"] = output["True-negatives"]/(output["True-negatives"]+output["False-positives"])
