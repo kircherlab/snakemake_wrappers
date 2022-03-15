fe0a5936a04e8b3b61f4ec9304fc55178c0ac3ed
visze <max.schubach@bihealth.de>
2022-03-15 15:08:13 +0100

    bugfix in correlate when having mult duplicates

--- evaluate/correlation/correlate.py
+++ evaluate/correlation/correlate.py
@@ -41,7 +41,7 @@ def cli(file_a, file_b, values, output_file):
     output = []
     methods = pd.Series(['pearson', 'spearman'], name="metric")
     for method in methods:
-        if df.drop_duplicates(keep=False).size == 0:
+        if df.drop_duplicates().shape[0] == 1:
             output.append(1.0)
         else:
             output.append(df.corr(method).loc["A", "B"])
